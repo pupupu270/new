bce8751d9dc9dd9e5bee71eddda67fde06fd0759
pupupu270 <lycee0@yahoo.co.jp>
2024-09-11 17:54:19 +0900

    Update index.js

--- index.js
+++ index.js
@@ -1,23 +1,3 @@
 
 
-let http = require("http");
-
-let fs = require("fs");
-
-
-http.createServer((req,res)=>{
-
-console.log(req.url);
-
-if(req.url =="/")
-req.url ="/index.html";
-
-let url = "node"+req.url;
-console.log(url);
-//fs.readFile("node/index.html",(err,data)=>{
-fs.readFile(url,(err,data)=>{
-res.writeHead(200,{"Content-Type":"text/html"});
-res.write(data);
-res.end();
-
-});
+console.log("hit");
